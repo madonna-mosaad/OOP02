21f8027308a39558eae60a11e8fdced28a9e88f0
madonna-mosaad <madonnamosaad86@gmail.com>
2024-07-08 18:47:50 +0300

    part02 as comment

--- OOP02/Program.cs
+++ OOP02/Program.cs
@@ -26,52 +26,52 @@ namespace OOP02
             #endregion
             #endregion
             #region part2
-            Employee[] employees = new Employee[3];
-            employees[0] = new Employee(10, "mina", SecurityPrivilege.DBA, 11000, new Date(1, 1, 2020), Gender.M);
-            employees[1] = new Employee(20, "maha", SecurityPrivilege.guest, 11000, new Date(10, 10, 2023), Gender.F);
-            employees[2] = new Employee(30, "omar", (SecurityPrivilege)15, 11000, new Date(1, 12, 2014), Gender.M);
+            //Employee[] employees = new Employee[3];
+            //employees[0] = new Employee(10, "mina", SecurityPrivilege.DBA, 11000, new Date(1, 1, 2020), Gender.M);
+            //employees[1] = new Employee(20, "maha", SecurityPrivilege.guest, 11000, new Date(10, 10, 2023), Gender.F);
+            //employees[2] = new Employee(30, "omar", (SecurityPrivilege)15, 11000, new Date(1, 12, 2014), Gender.M);
 
-            Employee temp;
-            if (employees != null)
-            {
-                for (int i = 0; i < employees.Length - 1; i++)
-                {
-                    for (int j = i + 1; j < employees.Length; j++)
-                    {
-                        employees[j].GetHireDate(out int day01, out int month01, out int year01);
-                        employees[i].GetHireDate(out int day02, out int month02, out int year02);
-                        if (year01 < year02)
-                        {
-                            temp = employees[i];
-                            employees[i] = employees[j];
-                            employees[j] = temp;
+            //Employee temp;
+            //if (employees != null)
+            //{
+            //    for (int i = 0; i < employees.Length - 1; i++)
+            //    {
+            //        for (int j = i + 1; j < employees.Length; j++)
+            //        {
+            //            employees[j].GetHireDate(out int day01, out int month01, out int year01);
+            //            employees[i].GetHireDate(out int day02, out int month02, out int year02);
+            //            if (year01 < year02)
+            //            {
+            //                temp = employees[i];
+            //                employees[i] = employees[j];
+            //                employees[j] = temp;
 
-                        }
-                        else if (year01 == year02 && month01 < month02)
-                        {
-                            temp = employees[i];
-                            employees[i] = employees[j];
-                            employees[j] = temp;
+            //            }
+            //            else if (year01 == year02 && month01 < month02)
+            //            {
+            //                temp = employees[i];
+            //                employees[i] = employees[j];
+            //                employees[j] = temp;
 
-                        }
-                        else if (year01 == year02 && month01 == month02 && day01 < day02)
-                        {
-                            temp = employees[i];
-                            employees[i] = employees[j];
-                            employees[j] = temp;
-                        }
-                    }
+            //            }
+            //            else if (year01 == year02 && month01 == month02 && day01 < day02)
+            //            {
+            //                temp = employees[i];
+            //                employees[i] = employees[j];
+            //                employees[j] = temp;
+            //            }
+            //        }
 
-                }
-            }
-            if (employees != null)
-            {
-                for (int i = 0; i < employees.Length; i++)
-                {
-                    Console.WriteLine(employees[i].ToString());
-                }
-            }
-            //While sorting 0 times Boxing and Unboxing process has occurred
+            //    }
+            //}
+            //if (employees != null)
+            //{
+            //    for (int i = 0; i < employees.Length; i++)
+            //    {
+            //        Console.WriteLine(employees[i].ToString());
+            //    }
+            //}
+            ////While sorting 0 times Boxing and Unboxing process has occurred
 
             #endregion
         }
